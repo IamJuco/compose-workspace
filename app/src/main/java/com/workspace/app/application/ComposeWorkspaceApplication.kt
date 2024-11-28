034be76a89f02a95b14e7b23e5a62e6e9acc5e72
IamJuco <jucoding99@gmail.com>
2024-11-28 19:54:28 +0900

    Chore: hiltAndroidApp 설정

---
+++ app/src/main/java/com/workspace/app/application/ComposeWorkspaceApplication.kt
@@ -0,0 +1,8 @@
+package com.workspace.app.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ComposeWorkspaceApplication : Application() {
+}
