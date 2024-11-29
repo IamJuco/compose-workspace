a3a51961b5322fb8cc12d737631b5ebb3681038d
IamJuco <jucoding99@gmail.com>
2024-11-29 17:39:32 +0900

    Chore: retrofit, okhttp, gson 의존성 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -42,6 +42,17 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.52")
     ksp("com.google.dagger:hilt-android-compiler:2.52")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // OkHttp3
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.11.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
