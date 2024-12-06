c31ef62adf96eaa149021d771fcbaa87bc0c0f1c
Juseonghyun <wntjdgus99@naver.com>
2024-12-06 23:30:31 +0900

    remove: ErrorHandling 변경으로 해당 파일 삭제

--- core/domain/src/main/java/com/workspace/core/domain/model/UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.workspace.core.domain.model
-
-import com.workspace.core.domain.api_error_handle.ApiException
-
-sealed interface UiState<out T> {
-    data object Loading : UiState<Nothing>
-    data class Success<T>(val data: T) : UiState<T>
-    data class Failure(val exception: ApiException) : UiState<Nothing>
-}
