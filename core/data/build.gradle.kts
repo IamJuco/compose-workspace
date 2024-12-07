31424f4cffadba4241c1b6697aab235f5312c9bf
Juseonghyun <wntjdgus99@naver.com>
2024-12-07 20:01:08 +0900

    chore: 각 기능별로 나눔

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
