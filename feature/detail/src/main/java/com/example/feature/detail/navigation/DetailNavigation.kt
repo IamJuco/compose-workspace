23620c9dedaac31af9a8cd5f4297617a7f7fa71c
Juseonghyun <wntjdgus99@naver.com>
2024-12-15 02:36:30 +0900

    refactor: Navigation 설정 변경에따라 관련 기능들 수정

--- feature/detail/src/main/java/com/example/feature/detail/navigation/DetailNavigation.kt
+++ feature/detail/src/main/java/com/example/feature/detail/navigation/DetailNavigation.kt
@@ -1,19 +1,28 @@
 package com.example.feature.detail.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import com.example.feature.detail.DetailRoute
 import com.workspace.core.domain.navigation.RouteModel
 
-fun NavGraphBuilder.detailRoute(navController: NavHostController) {
-    composable(
-        route = "${RouteModel.Detail.ROUTE}/{${RouteModel.Detail.ARG_POKEMON_ID}}",
-        arguments = listOf(navArgument(RouteModel.Detail.ARG_POKEMON_ID) { type = NavType.IntType })
-    ) { backStackEntry ->
-        val pokemonId = backStackEntry.arguments?.getInt(RouteModel.Detail.ARG_POKEMON_ID) ?: 0
-        DetailRoute(pokemonId = pokemonId, navController = navController)
+fun NavController.navigateToDetail(pokemonId: Int, navOptions: NavOptions? = null) {
+    navigate(RouteModel.Detail(pokemonId), navOptions)
+}
+
+fun NavGraphBuilder.detailNavGraph(
+    popBackStack: () -> Unit,
+    padding: PaddingValues
+) {
+    composable<RouteModel.Detail> { navBackStackEntry ->
+        val pokemonId = navBackStackEntry.toRoute<RouteModel.Detail>().pokemonId
+        DetailRoute(
+            pokemonId = pokemonId,
+            popBackStack = popBackStack,
+            padding = padding
+        )
     }
 }
