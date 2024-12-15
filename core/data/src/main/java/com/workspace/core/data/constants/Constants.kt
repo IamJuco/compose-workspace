f735b082ee00ff7da34be2cbb2a2a6c3ccfa7e60
Juseonghyun <wntjdgus99@naver.com>
2024-12-15 21:53:15 +0900

    feat: 나중에 사용할 user Token 저장할 pref 모듈 추가

--- core/data/src/main/java/com/workspace/core/data/constants/Constants.kt
+++ core/data/src/main/java/com/workspace/core/data/constants/Constants.kt
@@ -2,4 +2,5 @@ package com.workspace.core.data.constants
 
 object Constants {
     const val POKE_API_BASE_URL = "https://pokeapi.co/api/v2/"
+    const val APP_PREFERENCES_FILE = "app_preferences_file"
 }
---
+++ core/data/src/main/java/com/workspace/core/data/di/SharedPreferencesModule.kt
@@ -0,0 +1,28 @@
+package com.workspace.core.data.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.workspace.core.data.constants.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPreferencesModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences(Constants.APP_PREFERENCES_FILE, Context.MODE_PRIVATE)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferencesEditor(sharedPreferences: SharedPreferences): SharedPreferences.Editor {
+        return sharedPreferences.edit()
+    }
+}
