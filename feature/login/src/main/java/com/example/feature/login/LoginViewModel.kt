cd81982a915ed78f326e8af29f1b95e3d58c0459
Juseonghyun <wntjdgus99@naver.com>
2024-12-21 19:23:43 +0900

    refactor: StateFlow의 value 업데이트를 update 메서드로 변경

--- feature/login/src/main/java/com/example/feature/login/LoginViewModel.kt
+++ feature/login/src/main/java/com/example/feature/login/LoginViewModel.kt
@@ -9,23 +9,26 @@ import com.workspace.core.domain.usecase.LoginWithEmailUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginWithEmailUseCase: LoginWithEmailUseCase
-) : ViewModel(){
+) : ViewModel() {
     private val _loginWithEmailState = MutableStateFlow<UiState<User>>(UiState.Idle)
     val loginWithEmailState: StateFlow<UiState<User>> = _loginWithEmailState
 
     fun loginWithEmail(email: String, password: String) {
         viewModelScope.launch {
-            _loginWithEmailState.value = UiState.Loading
+            _loginWithEmailState.update { UiState.Loading }
             val result = loginWithEmailUseCase(email, password)
-            _loginWithEmailState.value = when (result) {
-                is ServiceResult.Success -> UiState.Success(result.data)
-                is ServiceResult.Error -> UiState.Error(result.errorCode, result.errorMessage)
+            _loginWithEmailState.update {
+                when (result) {
+                    is ServiceResult.Success -> UiState.Success(result.data)
+                    is ServiceResult.Error -> UiState.Error(result.errorCode, result.errorMessage)
+                }
             }
         }
     }
