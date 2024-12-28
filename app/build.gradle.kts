e8b178a794f2dd8c254f89eb5855c845209a98c3
Juseonghyun <jucoding99@gmail.com>
2024-12-29 01:05:55 +0900

    chore: buildTypes및 안쓰는 defaultConfig 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,15 +14,8 @@ plugins {
 
 android {
     namespace = "com.workspace.app"
-//    compileSdk = 35
 
     defaultConfig {
-//        applicationId = "com.workspace.main"
-//        minSdk = 24
-//        targetSdk = 35
-//        versionCode = 1
-//        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
