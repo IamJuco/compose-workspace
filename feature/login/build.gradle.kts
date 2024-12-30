131263dce1c18d5d5e772d0ba6c121e5a03060dd
Juseonghyun <jucoding99@gmail.com>
2024-12-31 04:36:39 +0900

    chore: 사용하지 않는 빌드 로직들 삭제

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -13,37 +13,6 @@ plugins {
 
 android {
     namespace = "com.example.feature.login"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
