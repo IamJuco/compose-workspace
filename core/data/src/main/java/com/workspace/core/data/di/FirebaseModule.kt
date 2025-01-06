9141b46d8a3ceaf7f8563309dd0c807cdcd5760a
Juseonghyun <jucoding99@gmail.com>
2025-01-07 03:05:58 +0900

    chore: Firestore 모듈 추가

--- core/data/src/main/java/com/workspace/core/data/di/FirebaseModule.kt
+++ core/data/src/main/java/com/workspace/core/data/di/FirebaseModule.kt
@@ -1,6 +1,10 @@
 package com.workspace.core.data.di
 
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,5 +17,9 @@ object FirebaseModule {
 
     @Provides
     @Singleton
-    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+    fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideFirestore(): FirebaseFirestore = Firebase.firestore
 }
